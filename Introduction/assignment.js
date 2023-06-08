1ffca158e744cc0dcfcc82d838a0f4b04305ed9a
Adetoyinbo Adeyemi <a.adeyemi@native.semicolon.africa>
2023-06-08 17:30:21 +0100

    initial commit

---
+++ Introduction/assignment.js
@@ -0,0 +1,24 @@
+var x = 2
+var y = 3
+var sum = x + y
+var twiceValue = sum * 2
+console.log(twiceValue)
+
+
+var certainValue = 27
+var remainder = certainValue % 10
+var final = remainder + certainValue
+console.log(final)
+
+var x = 2
+var n = 4
+var resultingValue = x * (x * n)
+console.log(resultingValue)
+
+var a = 20
+var x = a + 10
+var y = a - 10
+var z = a % 10
+product = x * y * z
+certainValue2= a/product
+console.log(certainValue2)
--- Introduction/variables.js
+++ Introduction/variables.js
@@ -1,10 +1,28 @@
-var number = 20;
-let age = 23;
-const SCHOOL = "Semicolon Africa";
+// var number = 20;
+// let age = 23;
+// const SCHOOL = "Semicolon Africa";
 
-age = 25
-SCHOOL ="Alt School";
+// age = 25
+// // SCHOOL ="Alt School";
 
-console.log(age);
-console.log(number)
-console.log(SCHOOL)
+// console.log(age);
+// console.log(number)
+// console.log(SCHOOL)
+
+// var var1 = 2
+// var var2 = 4
+// var var3 = 6
+// var var4
+
+// var var4 = var1
+// var var1 = var2
+// var var2 = var3
+// var var3 = var4
+// console.log(var1)
+// console.log(var2)
+// console.log(var3)
+
+// var condition = false;
+// condition ? console.log("True") : console.log("False")
+// console.log(false && !false)
+
